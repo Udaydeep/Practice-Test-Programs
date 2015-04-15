574ea48c1357822973f793203cf85a42cd22517b
Uday <udaydeep.thota@gmail.com>
2015-04-15 01:19:09 -0700

    Added 2nd File
    Test

---
+++ tests/src/main/java/com/bayamp/string/tests/MyStringTests.java
@@ -0,0 +1,25 @@
+package com.bayamp.string.tests;
+
+import org.testng.annotations.Test;
+
+public class MyStringTests {
+	
+	@Test
+	public void positiveScenario1(){
+		System.out.println("scenari1");
+		
+	}
+	
+	@Test
+	public void positiveScenario2(){
+		
+		System.out.println("scenari2");
+	}
+	
+	@Test
+	public void positiveScenario3(){
+		System.out.println("scenari3");
+		
+	}
+
+}
